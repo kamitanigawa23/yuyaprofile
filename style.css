1eece34af6acb73f95291ee8051afac7a214e0fa
kamitanigawa23 <69797949+kamitanigawa23@users.noreply.github.com>
2020-10-10 17:18:53 +0900

    Create style.css

---
+++ style.css
@@ -0,0 +1,161 @@
+# yuyaprofile
+プロフィール
+@charset "utf-8";
+/* 共通部分 */
+html{
+  font-size: 100%;
+}
+body{
+  font-family: "Yu Gothic Medium","遊ゴシック Medium","YuGothic","遊ゴシック体","ヒラギノ角ゴ Pro W3",sans-serif;
+  line-height: 1.7;
+  color: #432;
+}
+a{
+  text-decoration: none;
+}
+img{
+  max-width: 100%;
+}
+.logo{
+  width: 210px;
+  margin-top: 14px;
+}
+.main-nav{
+  display: flex;
+  font-size: 1.25rem;
+  text-transform: uppercase;
+  margin-top: 34px;
+  list-style: none;
+}
+.main-nav li{
+  margin-left: 36px;
+}
+.main-nav a{
+  color: #432;
+}
+.main-nav a:hover{
+  color: #0bd;
+}
+.page-header{
+  display: flex;
+  justify-content: space-between;
+}
+.wrapper{
+  max-width: 1100px;
+  margin: 0 auto;
+  padding: 0 4%;
+}
+.home-content{
+  text-align: center;
+  margin-top: 10%;
+}
+.home-content p {
+  font-size: 1.125rem;
+  margin: 10px 0 42px;
+}
+.page-title{
+  font-size: 5rem;
+  font-family: "Philosopher", serif;
+  text-transform: uppercase;
+  font-weight: normal;
+}
+.button{
+  font-size: 1.375rem;
+  background: #0bd;
+  color: #fff;
+  border-radius: 5px;
+  padding: 18px 32px;
+}
+.button:hover{
+  background: #0090aa;
+}
+.big-bg{
+  background-size: cover;
+  background-position: center top;
+  background-repeat: no-repeat;
+}
+#home{
+  background-image: url(TSU92_shizukugaturanarumilk_TP_V.jpg);
+  min-height: 100vh;
+}
+#home .page-title{
+  text-transform: none;
+}
+#Profile{
+  background-image: url(shikun5V7A1829_TP_V.jpg);
+  height: 270px;
+  margin-bottom: 40px;
+}
+#Profile .page-title{
+  text-align: center;
+}
+footer{
+  background: #432;
+  text-align: center;
+  padding: 26px 0;
+}
+footer p{
+  color: #fff;
+  font-size: 0.875rem;
+}
+article{
+  width: 74%;
+}
+aside{
+  width: 22%;
+}
+.profile-contents{
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 50px;
+}
+article img{
+  margin-bottom: 20px;
+}
+article p{
+  margin-bottom: 1rem;
+}
+.post-title{
+  font-family: "Yu Mincho","YuMincho",serif;
+  font-size: 2rem;
+  font-weight: normal;
+  /* margin-left: 120px; */
+}
+.sub-title{
+  font-size: 1.375rem;
+  padding: 0 8px 8px;
+  border-bottom: 2px #0bd solid;
+  font-weight: normal;
+}
+.sub-menu{
+  margin-bottom: 60px;
+  list-style: none;
+}
+.sub-menu li{
+  border-bottom: 1px #ddd solid;
+}
+.sub-menu a{
+  color: #432;
+  padding: 10px;
+  display: block;
+}
+.sub-menu a:hover{
+  color: #0bd;
+}
+
+/* モバイル版 */
+@media (max-width: 600px){
+  .page-title{
+    font-size: 2.5rem;
+  }
+}
+.main-nav{
+  font-size: 1rem;
+  margin-top: 10px;
+}
+.main-nav li{
+  margin: 0 20px;
+}
+.home-content{
+  margin-top: 20%;
+}
